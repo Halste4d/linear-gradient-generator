287ed28b0afc221f1f052fd44d5f6cd90c45bbc6
TedBrincadeira <55673351+TedBrincadeira@users.noreply.github.com>
2020-10-21 20:37:42 -0300

    Update index.js
    Reduzindo código repetido.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ function el(el){
   return document.querySelector(el)
 }
 
+function setEvent(ele){
+  el(ele).addEventListener("input", applyBackgroundGradient);
+}
+
 function applyBackgroundGradient() { 
     el('body').style.background = `linear-gradient(to right, ${el('.firstColor').value}, ${el('.secondColor').value})`
-  
     el('p').textContent = el('body').style.background + ";"; 
     el('#colorCode').textContent = `Color Code: ${el('.firstColor').value}; ${el('.secondColor').value};`
     el("#inputText").value = el("#cssCode").innerText;
-} 
-
-el('.firstColor').addEventListener("input", applyBackgroundGradient);   
-el('.secondColor').addEventListener("input", applyBackgroundGradient);
-
+}
 
 function copyToClipboard() {
-  var copyText = el("#cssCode").innerText;
-  var copyText2 = el("#inputText");
+  const copyText = el("#cssCode").innerText,
+  copyText2 = el("#inputText");
   try {
     navigator.clipboard.writeText(copyText)
   } catch(e) {
@@ -24,3 +23,6 @@ function copyToClipboard() {
     document.execCommand('copy')
   }
 } 
+
+setEvent('.firstColor')
+setEvent('.secondColor')
